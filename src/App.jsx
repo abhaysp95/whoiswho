22d18839443f44e9027b0037b1ecb57c74d2f694
abhay <abhaysp95@protonmail.com>
2023-07-03 18:28:28 +0530

    feat: making basic jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 const App = () => {
 	return (
 		<div>
-			Hello World!!!
+			<Header />
+			<Content />
+			<Footer />
 		</div>
 	)
 };
