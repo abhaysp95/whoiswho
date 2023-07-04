869b315c9e2e761dfe29471b3d96fa32084e5192
abhay <abhaysp95@protonmail.com>
2023-07-04 12:00:35 +0530

    feat: passing data as props from App.jsx to child components

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,13 @@
 const Footer = () => {
 	return (
-		<header>
-			<h1>Footer</h1>
-		</header>
+		<div className="container">
+			<div className="footer row justify-content-center mt-3 mb-4">
+				<div className="col-8">
+					<h5>WhoIsWho - {new Date().getFullYear()}</h5>
+					<p style={{fontSize: "0.75rem"}}>icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+				</div>
+			</div>
+		</div>
 	)
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,14 @@
-const Header = () => {
+const Header = ({currentTeam, employeeCount}) => {
+	var today = new Date()
+
 	return (
-		<header>
-			<h1 className="mt-4 p5 bg-primary rounded text-white">Header</h1>
+		<header className="container">
+			<div className="row justify-content-center mt-3 mt-3 mb-4">
+				<div className="col-8">
+					<h1>WhoIsWho</h1>
+					<h3>Team {currentTeam} have {employeeCount} members</h3>
+				</div>
+			</div>
 		</header>
 	)
 }
