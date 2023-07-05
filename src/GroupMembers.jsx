7e22bca887a65faa0619d9192072c263a43e36af
abhay <abhaysp95@protonmail.com>
2023-07-05 22:09:58 +0530

    feat: dummy container for member group

---
+++ src/GroupMembers.jsx
@@ -0,0 +1,13 @@
+const GroupMembers = () => {
+	return (
+		<main className="container">
+			<div className="row justify-content-center mt-3 mb-3">
+				<div className="col-8">
+					<h3>Grouped team members</h3>
+				</div>
+			</div>
+		</main>
+	)
+}
+
+export default GroupMembers
