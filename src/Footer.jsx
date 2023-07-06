be4d2bf0aabc2d95fe0c0f0ce46093cdbd23486e
abhay <abhaysp95@protonmail.com>
2023-07-06 14:59:55 +0530

    feat: make footer position fixed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
 	return (
-		<div className="container">
-			<div className="footer row justify-content-center mt-3 mb-4">
+		<div className="container" style={{position: "fixed", bottom: "0", left: "0"}}>
+			<div className="footer row justify-content-center mt-3">
 				<div className="col-8">
 					<h5>WhoIsWho - {new Date().getFullYear()}</h5>
-					<p style={{fontSize: "0.75rem"}}>icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+					<p style={{fontSize: "0.75rem"}} className="text-end">icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
 				</div>
 			</div>
 		</div>
